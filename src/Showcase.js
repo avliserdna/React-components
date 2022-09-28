c3389493fd5f62b204db0c2ec99cba99bf11913b
Andre Silva <andre.christian.silva@gmail.com>
2022-09-28 04:44:46 +0000

    Finished Click events and BaseStats

--- src/Showcase.js
+++ src/Showcase.js
@@ -1,27 +1,27 @@
-import bulbasaur from './images/bulbasaur.jpg';
-import './Showcase.css';
+import bulbasaur from "./images/bulbasaur.jpg";
+import "./Showcase.css";
 
 const Showcase = () => {
-  const favPokemon = 'Bulbasaur';
+  const favPokemon = "Bulbasaur";
 
   const pokeCharacteristics = {
-    type: 'Grass',
-    move: 'Vine Whip'
+    type: "Grass",
+    move: "Vine Whip",
   };
 
   return (
-    <div className='showcase-wrapper'>
+    <div className="showcase-wrapper">
       <h1>{favPokemon}'s Showcase Component</h1>
       <img
-        style={{ height: 500, width: 500, padding: 20, borderRadius: '50%' }}
+        style={{ height: 500, width: 500, padding: 20, borderRadius: "50%" }}
         src={bulbasaur}
-        alt='bulbasaur'
+        alt="bulbasaur"
       />
       <h2>
-        {favPokemon}'s type is{' '}
-        <span className='poke-type'>{pokeCharacteristics.type}</span> and one of
-        their moves is{' '}
-        <span className='poke-move'>{pokeCharacteristics.move}</span>.
+        {favPokemon}'s type is{" "}
+        <span className="poke-type">{pokeCharacteristics.type}</span> and one of
+        their moves is{" "}
+        <span className="poke-move">{pokeCharacteristics.move}</span>.
       </h2>
     </div>
   );
